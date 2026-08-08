7c26d8d5276f645b864c8a01cc84a116d83f8dc1
Redwan Ahmed Tapu <121452952+RedwanAhmedTapu@users.noreply.github.com>
2026-08-09 02:28:48 +0600

    food delivery implemented

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -70,7 +70,7 @@ export function Footer() {
               width="100%"
               height="100%"
               style={{ border: 0 }}
-              allowFullScreen=""
+              allowFullScreen={true}
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
